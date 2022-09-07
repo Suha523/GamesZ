4633051820b61bb21867a2559441dea1836fd3f4
suha <suhashehahde@gmail.com>
2022-09-07 00:56:03 -0700

    add css responspilty of screens

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import '../assets/styles/home.css'
+import React from 'react';
+import '../assets/styles/home.css';
 export default function Home() {
-  return (
-    <div className='home'></div>
-  )
+    return (
+        <div className="home">
+            <div className='games-intro'>
+                <h1>ASAZ Games</h1>
+                <p className='into'>Playing without limits</p>
+            </div>
+            <div className="overlay"></div>
+        </div>
+    );
 }
